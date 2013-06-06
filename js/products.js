22d533603e7163c0b8cb5193efa5b7e4e8c102a0
Ken <dee.mannn@gmail.com>
2013-06-06 16:27:54 -0700

    - Updated product navigation javascript to be more accurate - Added more to the details page - fixed border issue in IE - Modified and added styling

--- js/products.js
+++ js/products.js
@@ -1,40 +1,48 @@
 $( document ).ready(function() {
 	
-    $("#all-button").hover(
-	  function () {
-	    $(this).attr('src', 'img/All-Selected.png');
-	  },
-	  function () {
-	    $(this).attr('src', 'img/All--NOT-Selected.png');
-	  }
-	);
+	if (!$("#all-button").hasClass("selected")) {
+		$("#all-button").hover(
+		  function () {
+		    $(this).children("img").attr('src', 'img/All-Selected.png');
+		  },
+		  function () {
+		    $(this).children("img").attr('src', 'img/All--NOT-Selected.png');
+		  }
+		);
+	}
+    
+	if (!$("#hat-button").hasClass("selected")) {
+		$("#hat-button").hover(
+		  function () {
+		    $(this).children("img").attr('src', 'img/Hat-Selected.png');
+		  },
+		  function () {
+		    $(this).children("img").attr('src', 'img/Hat--NOT-Selected.png');
+		  }
+		);
+	}
 	
-	$("#hat-button").hover(
-	  function () {
-	    $(this).attr('src', 'img/Hat-Selected.png');
-	  },
-	  function () {
-	    $(this).attr('src', 'img/Hat--NOT-Selected.png');
-	  }
-	);
+	if (!$("#beanie-button").hasClass("selected")) {
+		$("#beanie-button").hover(
+		  function () {
+		    $(this).children("img").attr('src', 'img/Beanie-Selected.png');
+		  },
+		  function () {
+		    $(this).children("img").attr('src', 'img/Beanie--NOT-Selected.png');
+		  }
+		);
+	}
 	
-	$("#beanie-button").hover(
-	  function () {
-	    $(this).attr('src', 'img/Beanie-Selected.png');
-	  },
-	  function () {
-	    $(this).attr('src', 'img/Beanie--NOT-Selected.png');
-	  }
-	);
-	
-	$("#cap-button").hover(
-	  function () {
-	    $(this).attr('src', 'img/Cap-Selected.png');
-	  },
-	  function () {
-	    $(this).attr('src', 'img/Cap--NOT-Selected.png');
-	  }
-	);
+	if (!$("#cap-button").hasClass("selected")) {
+		$("#cap-button").hover(
+		  function () {
+		    $(this).children("img").attr('src', 'img/Cap-Selected.png');
+		  },
+		  function () {
+		    $(this).children("img").attr('src', 'img/Cap--NOT-Selected.png');
+		  }
+		);
+	}
     
     var currentProduct = null;
     $(".product").hover(hoverProduct, unhoverProduct);
