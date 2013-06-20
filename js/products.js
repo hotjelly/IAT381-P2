c566e60f9d15895c424ff56f3b1a95bd37724c20
Ken <dee.mannn@gmail.com>
2013-06-20 13:33:55 -0700

    - Changed product category navigation to work with just images and not text as well - Added pages for each category - Added additional images to new-arrivals page - Small additions and changes to other stylings

--- js/products.js
+++ js/products.js
@@ -1,49 +1,5 @@
 $( document ).ready(function() {
 	
-	if (!$("#all-button").hasClass("selected")) {
-		$("#all-button").hover(
-		  function () {
-		    $(this).children("img").attr('src', 'img/All-Selected.png');
-		  },
-		  function () {
-		    $(this).children("img").attr('src', 'img/All--NOT-Selected.png');
-		  }
-		);
-	}
-    
-	if (!$("#hat-button").hasClass("selected")) {
-		$("#hat-button").hover(
-		  function () {
-		    $(this).children("img").attr('src', 'img/Hat-Selected.png');
-		  },
-		  function () {
-		    $(this).children("img").attr('src', 'img/Hat--NOT-Selected.png');
-		  }
-		);
-	}
-	
-	if (!$("#beanie-button").hasClass("selected")) {
-		$("#beanie-button").hover(
-		  function () {
-		    $(this).children("img").attr('src', 'img/Beanie-Selected.png');
-		  },
-		  function () {
-		    $(this).children("img").attr('src', 'img/Beanie--NOT-Selected.png');
-		  }
-		);
-	}
-	
-	if (!$("#cap-button").hasClass("selected")) {
-		$("#cap-button").hover(
-		  function () {
-		    $(this).children("img").attr('src', 'img/Cap-Selected.png');
-		  },
-		  function () {
-		    $(this).children("img").attr('src', 'img/Cap--NOT-Selected.png');
-		  }
-		);
-	}
-    
     var currentProduct = null;
     $(".product").hover(hoverProduct, unhoverProduct);
     
@@ -64,7 +20,5 @@ $( document ).ready(function() {
     			}
 		});
     }
-    
-   
     
 });
